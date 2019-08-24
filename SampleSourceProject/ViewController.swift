d50358672c982c0ade60d8d4e8daac120e57dd2e
佐々木 栄人 <sasaki@freemake.co.jp>
2019-08-24 20:54:24 +0900

    Initial Commit

---
+++ SampleSourceProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSourceProject
+//
+//  Created by 佐々木 栄人 on 2019/08/24.
+//  Copyright © 2019 beams001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
